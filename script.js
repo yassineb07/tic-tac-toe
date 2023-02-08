85c3621f2e2e529c462723fbc3f4aad987f416e4
Yassine <progyss9@gmail.com>
2023-02-08 22:03:06 +0100

    Check for win or tie

--- script.js
+++ script.js
@@ -20,12 +20,38 @@ const game = (() => {
     }
   };
 
+  const checkWin = () => {
+    const winingCombinations = [
+      [boardArray[0], boardArray[1], boardArray[2]],
+      [boardArray[3], boardArray[4], boardArray[5]],
+      [boardArray[6], boardArray[7], boardArray[8]],
+      [boardArray[0], boardArray[3], boardArray[6]],
+      [boardArray[1], boardArray[4], boardArray[7]],
+      [boardArray[2], boardArray[5], boardArray[8]],
+      [boardArray[0], boardArray[4], boardArray[8]],
+      [boardArray[2], boardArray[4], boardArray[6]],
+    ];
+    return winingCombinations.some((combination) =>
+      combination.every((el) => el === combination[0] && el !== null)
+    );
+  };
+  const checkTie = () => boardArray.every((el) => el !== null);
+
+  const isGameOver = () => {
+    if (checkWin()) {
+      console.log(`winner : ${currentPlayer.getName()}`);
+    } else if (checkTie()) {
+      console.log('Tie');
+    }
+  };
+
   const play = (target) => {
     const index = +target.getAttribute('id');
     if (boardArray[index] === 'O' || boardArray[index] === 'X') {
       return;
     }
     boardArray.splice(index, 1, currentPlayer.getSymbol());
+    isGameOver();
     switchTurn(currentPlayer);
   };
 
